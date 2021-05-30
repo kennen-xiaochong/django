2c647d942a780d3aeb360ef0d6f603d6d6a818d1
kennen-xiaochong <373062231@qqc.com>
2021-05-30 19:44:39 +0800

    5.21-del_dep_old finished 5.30-version

--- myapp/urls.py
+++ myapp/urls.py
@@ -13,10 +13,10 @@ urlpatterns=[
     path('edit_dep_old/<int:dep_id>/',edit_dep_old),
 
     #操作团体数据表路由
-#    path('add_group_old/',add_group_old),
-#    path('list_group_old/',list_group_old),
-#    path('del_group_old/<int:group_id>',del_group_old),
-#    path('edit_group_old/<int:group_id>/',edit_grou_old),
+    path('add_group_old/',add_group_old),
+    path('list_group_old/',list_group_old),
+    path('del_group_old/<int:group_id>',del_group_old),
+    path('edit_group_old/<int:group_id>/',edit_grou_old),
     #补充数据信息表
 #    path('add_employeeinfo_old/',add_employeeinfo_old),
 #    path('list_employeeinfo_old/',list_employeeinfo_old),
--- myapp/views.py
+++ myapp/views.py
@@ -27,22 +27,42 @@ def add_dep_old(request):
             pass
     return render(request,'add_dep_old.html')
 
-def edit_dep_old(request):
+def edit_dep_old(request,dep_id):
+    dep_object = department.objects.get(id=dep_id)
     if request.method=='POST':
         dep_name=request.POST.get('dep_name')
         dep_script=request.POST.get('dep_script')
         if dep_name.strip()=='':
-            return render(request,'add_dep_old.html',{'error_info':'部门不能为空！'})
+            return render(request,'edit_dep_old.html',{'error_info':'部门不能为空！'})
         try:
-            p=department.objects.create(dep_name=dep_name,dep_script=dep_script)
+            p=department.objects.filter(id=dep_id).update(dep_name=dep_name,dep_script=dep_script)
             return redirect('/test_orm_old/list_dep_old')
         except Exception as e:
-            return render(request,'add_dep_old.html',{'error_info':'输入部门名称重复或有误!'})
+            return render(request,'edit_dep_old.html',{'error_info':'输入部门名称重复或有误!'})
         finally:
             pass
-    return render(request,'edit_dep_old.html')
+    return render(request,'edit_dep_old.html',{'department':dep_object})
 
 def del_dep_old(request,dep_id):
     department.objects.filter(id=dep_id).delete()
     return redirect('/test_orm_old/list_dep_old')
+
+def list_group_old(request):
+    group_list=group.objects.all()
+    return render(request,'list_group_old.html',{'group_list':group_list})
+
+def add_group_old(request):
+    if request.method=="POST":
+        group_name=request.POST.get('group_name')
+        group_script=request.POST.get('group_script')
+        if group_name.strip()==''
+            render(request,'test_orm_old/add_dep_old.html',{'error_info':'团体不能为空')
+        try:
+            g=group.objects.create(group_name=group_name,group_list=group_script)
+            return redirect('test_orm_old/list_group_old')
+        except Exception as e:
+            return render(request,'add_grup_old.html',{'error_info':'输入部门名称重复或有误!'})
+        finally:
+            pass
+    return render(request,'list_group_old.html')
 
